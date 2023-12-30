678323ad4d21c331538b0ffa580a90b291823e5d
Bartek <bartekrycko@gmail.com>
2023-12-30 19:33:22 +0100

    Rename + add batched weights example in pytorch

--- python/icp_3d/point_to_point_pytorch_three_weights_batch_3d.py
+++ python/icp_3d/point_to_point_pytorch_three_weights_batch_3d.py
@@ -8,15 +8,32 @@ import point_to_point_utils_3d as utils
 class myLayerRotation(nn.Module):
     def __init__(self):
         super().__init__()
-        weight = torch.Tensor(3,3)
+        weight = torch.Tensor(3,1) #RADIANS
         self.weight = nn.Parameter(weight, requires_grad=True)
         torch.nn.init.zeros_(self.weight)
 
     def forward(self, x):
-        a = torch.zeros_like(x)
+        z = torch.zeros_like(x)
         for i in range(len(x)):
-            a[i] = self.weight @ x[i]
-        return a
+            a = self.weight[0]
+            b = self.weight[1]
+            c = self.weight[2]
+
+            r00 = torch.cos(c) * torch.cos(b)
+            r01 = torch.sin(a) * torch.sin(b) * torch.cos(c) - torch.cos(a) * torch.sin(c)
+            r02 = torch.cos(a) * torch.sin(b) * torch.cos(c) + torch.sin(a) * torch.sin(c)
+            r10 = torch.cos(b) * torch.sin(c)
+            r11 = torch.sin(a) * torch.sin(b) * torch.sin(c) + torch.cos(a) * torch.cos(c)
+            r12 = torch.cos(a) * torch.sin(b) * torch.sin(c) - torch.sin(a) * torch.cos(c)
+            r20 = -torch.sin(b)
+            r21 = torch.sin(a) * torch.cos(b)
+            r22 = torch.cos(a) * torch.cos(b)
+
+            x_0 = r00 * x[i][0] + r01 * x[i][1] + r02 * x[i][2]
+            x_1 = r10 * x[i][0] + r11 * x[i][1] + r12 * x[i][2]
+            x_2 = r20 * x[i][0] + r21 * x[i][1] + r22 * x[i][2]
+            z[i] = torch.cat((x_0, x_1, x_2), 0)
+        return z
 
 class myLayerTranslation(nn.Module):
     def __init__(self):
@@ -62,14 +79,17 @@ if __name__ == '__main__':
     R = scm[0].weight
     T = scm[1].weight
 
-    alpha1 = torch.atan2(R[2, 1], R[2, 2])
-    alpha2 = torch.atan2(-R[2, 0], torch.sqrt(R[2, 1]**2 + R[2, 2]**2))
-    alpha3 = torch.atan2(R[1, 0], R[0, 0])
+    alpha1 = R[0][0]
+    alpha2 = R[1][0]
+    alpha3 = R[2][0]
+    print(f"R is: {R}")
     print(f"DEG: {utils.rad_to_deg(alpha1)}, {utils.rad_to_deg(alpha2)} {utils.rad_to_deg(alpha3)}")
 
     pc_corrected = torch.zeros_like(pc_fixed, requires_grad=False)
-    for i in range(len(pc_fixed)):
-        pc_corrected[i] = R @ pc_rotated[i] + T
+    with torch.no_grad():
+        R = torch.from_numpy(utils.euler_angles_to_rotation_matrix([alpha1, alpha2, alpha3]))
+        for i in range(len(pc_fixed)):
+            pc_corrected[i] = R @ pc_rotated[i] + T
     utils.plot_3d_point_clouds(
         (np.array(pc_fixed.detach().numpy()), "fixed"),
         (np.array(pc_corrected.detach().numpy()), "corrected"),
